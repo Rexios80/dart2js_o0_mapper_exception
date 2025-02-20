4d22f871145f3ca6674365f8acb70f9b07f5dc37
Rexios <rexios@rexios.dev>
2025-02-20 14:30:27 -0500

    Initial commit

---
+++ bin/dart2js_o0_mapper_exception.dart
@@ -0,0 +1,5 @@
+import 'package:dart2js_o0_mapper_exception/dart2js_o0_mapper_exception.dart' as dart2js_o0_mapper_exception;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart2js_o0_mapper_exception.calculate()}!');
+}
---
+++ test/dart2js_o0_mapper_exception_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart2js_o0_mapper_exception/dart2js_o0_mapper_exception.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
